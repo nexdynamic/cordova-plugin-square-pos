2b62d34829ace305651e267ffc025bf23fbf41cf
Mark Slater <mark@nexdynamic.com>
2021-10-15 12:13:41 +0100

    Remove unneeded refs

--- src/ios/SquarePOS.h
+++ src/ios/SquarePOS.h
@@ -1,8 +1,7 @@
 #import <Cordova/CDV.h>
-@import SquarePointOfSaleSDK;
 
 @interface SquarePOS : CDVPlugin
     @property   NSString* callbackId;
-    @property (nonatomic, assign) SCCAPIRequestTenderTypes supportedTenderTypes;
     - (void)initTransaction:(CDVInvokedUrlCommand*)command;
 @end
+
